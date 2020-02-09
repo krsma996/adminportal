059d5292536a5294ebbea561890604374a5114ee
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-02-09 23:05:09 +0100

    Added new features
    Added HomeController root path to admin page
    Edited login.html file added new form for admin login and password
    Added from Bookstore class SecurityConfig deleted root path "/"
    Added from Bookstore class Utility copy paste
    Added from Bookstore class UserReposityory to and defined findbyUsername
    Added UserSecurityService impl copy paste

--- adminportal/src/main/java/com/adminportal/controller/HomeController.java
+++ adminportal/src/main/java/com/adminportal/controller/HomeController.java
@@ -8,8 +8,19 @@ public class HomeController {
 
 	
 	@RequestMapping("/")
+	public String index() {
+		return"redirect:/home";
+	}
+	
+	@RequestMapping("/home")
 	public String home() {
 		return"home";
+	}
+	
+	
+	@RequestMapping("login")
+	public String login() {
+		return"login";
 	}
 	
 }
