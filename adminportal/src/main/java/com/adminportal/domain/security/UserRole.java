f9a1bb22824628c310e83dd0802a18f1261af7d6
NikolaKrsmanovic1996 <nikolakrsmanovic223@gmail.com>
2020-04-20 12:02:09 +0200

    Added new features
    Added login.html(front page) for admin user
    Added header.html
    fixed jar version in pom.xml
    UserRole Class fixed
    Fixed primary key duplicate entry need to have same spring boot versions

--- adminportal/src/main/java/com/adminportal/domain/security/UserRole.java
+++ adminportal/src/main/java/com/adminportal/domain/security/UserRole.java
@@ -17,7 +17,7 @@ import com.adminportal.domain.User;
 public class UserRole {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long userRoleId;
 	
 	@ManyToOne(fetch = FetchType.EAGER)
